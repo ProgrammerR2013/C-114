17bcababe2e1d67691cb651bb9e5fda3d1c2a596
ProgrammerR2013 <98456890+ProgrammerR2013@users.noreply.github.com>
2023-04-10 15:42:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(480, 360);
+    canvas.center();
+}
+
+function draw() {
+}
+
+function take_snapshot() {
+    save("myMoustacheImage.png");
+}
